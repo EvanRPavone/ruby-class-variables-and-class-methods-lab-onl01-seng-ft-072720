0d729e6490f19fcfda1058aab560267f321a846d
Evan Pavone <evanrpavone@gmail.com>
2020-08-06 01:35:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Song
   attr_accessor :name, :artist, :genre
   @@count = 0
-  def initialize(name, artist, genre, genres, artists)
+  def initialize(name, artist, genre)
     @@count +=1
     @name = name
     @artist = artist
